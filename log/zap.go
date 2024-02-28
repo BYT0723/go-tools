2ff3c476b817b04e13291be0067e12314af125e9
BYT0723 <twang9739@gmail.com>
2024-02-29 00:49:24 +0800

    fix: log args type

--- log/zap.go
+++ log/zap.go
@@ -74,87 +74,87 @@ func Init(opts ...Option) {
 }
 
 func Debug(args ...any) {
-	l.logger.Sugar().Debug(args)
+	l.logger.Sugar().Debug(args...)
 }
 
 func Debugf(msg string, args ...any) {
-	l.logger.Sugar().Debugf(msg, args)
+	l.logger.Sugar().Debugf(msg, args...)
 }
 
 func Debugw(msg string, keyValues ...any) {
-	l.logger.Sugar().Debugw(msg, keyValues)
+	l.logger.Sugar().Debugw(msg, keyValues...)
 }
 
 func Info(args ...any) {
-	l.logger.Sugar().Info(args)
+	l.logger.Sugar().Info(args...)
 }
 
 func Infof(msg string, args ...any) {
-	l.logger.Sugar().Infof(msg, args)
+	l.logger.Sugar().Infof(msg, args...)
 }
 
 func Infow(msg string, keyValues ...any) {
-	l.logger.Sugar().Infow(msg, keyValues)
+	l.logger.Sugar().Infow(msg, keyValues...)
 }
 
 func Warn(args ...any) {
-	l.logger.Sugar().Warn(args)
+	l.logger.Sugar().Warn(args...)
 }
 
 func Warnf(msg string, args ...any) {
-	l.logger.Sugar().Warnf(msg, args)
+	l.logger.Sugar().Warnf(msg, args...)
 }
 
 func Warnw(msg string, keyValues ...any) {
-	l.logger.Sugar().Warnw(msg, keyValues)
+	l.logger.Sugar().Warnw(msg, keyValues...)
 }
 
 func Error(args ...any) {
-	l.logger.Sugar().Error(args)
+	l.logger.Sugar().Error(args...)
 }
 
 func Errorf(msg string, args ...any) {
-	l.logger.Sugar().Errorf(msg, args)
+	l.logger.Sugar().Errorf(msg, args...)
 }
 
 func Errorw(msg string, keyValues ...any) {
-	l.logger.Sugar().Errorw(msg, keyValues)
-}
-
-func Fatal(args ...any) {
-	l.logger.Sugar().Fatal(args)
-}
-
-func Fatalf(msg string, args ...any) {
-	l.logger.Sugar().Fatalf(msg, args)
-}
-
-func Fatalw(msg string, keyValues ...any) {
-	l.logger.Sugar().Fatalw(msg, keyValues)
+	l.logger.Sugar().Errorw(msg, keyValues...)
 }
 
 func DPanic(args ...any) {
-	l.logger.Sugar().DPanic(args)
+	l.logger.Sugar().DPanic(args...)
 }
 
 func DPanicf(msg string, args ...any) {
-	l.logger.Sugar().DPanicf(msg, args)
+	l.logger.Sugar().DPanicf(msg, args...)
 }
 
 func DPanicw(msg string, keyValues ...any) {
-	l.logger.Sugar().DPanicw(msg, keyValues)
+	l.logger.Sugar().DPanicw(msg, keyValues...)
 }
 
 func Panic(args ...any) {
-	l.logger.Sugar().Panic(args)
+	l.logger.Sugar().Panic(args...)
 }
 
 func Panicf(msg string, args ...any) {
-	l.logger.Sugar().Panicf(msg, args)
+	l.logger.Sugar().Panicf(msg, args...)
 }
 
 func Panicw(msg string, keyValues ...any) {
-	l.logger.Sugar().Panicw(msg, keyValues)
+	l.logger.Sugar().Panicw(msg, keyValues...)
+}
+
+func Fatal(args ...any) {
+	l.logger.Sugar().Fatal(args...)
+}
+
+func Fatalf(msg string, args ...any) {
+	l.logger.Sugar().Fatalf(msg, args...)
+}
+
+func Fatalw(msg string, keyValues ...any) {
+	l.logger.Sugar().Fatalw(msg, keyValues...)
 }
 
 func ZapLogger() *zap.Logger {
