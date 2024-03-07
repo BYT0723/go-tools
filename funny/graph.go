463f2d7f8f4e30388711652a8868efef45055893
BYT0723 <twang9739@gmail.com>
2024-03-07 20:01:41 +0800

    feat: graph init

--- funny/graph.go
+++ funny/graph.go
@@ -1,24 +1,66 @@
 package funny
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-type function func(variables ...float64) bool
+type BinaryFunction func(x, y float64) bool
 
-func Draw(f function) error {
+type Graph struct {
+	XScale float64
+	YScale float64
+	Edge   *Edge
+}
+
+type Edge struct {
+	// 原点字符
+	Zero string
+	// X轴
+	X string
+	// Y轴
+	Y string
+	// 符合函数的字符
+	FC string
+	// 刻度线
+	Scale string
+}
+
+var defaultEdge = &Edge{Zero: "+", X: "-", Y: "|", FC: "*", Scale: "+"}
+
+func (g *Graph) Draw(fc BinaryFunction) error {
 	size, err := GetTermSize()
 	if err != nil {
 		return err
 	}
-	var (
-		maxRow = size.Row
-		maxCol = size.Col
-	)
+
+	if g.Edge == nil {
+		g.Edge = defaultEdge
+	}
 
 	// 打印爱心形状
-	for row := maxRow / 2; row > 0-maxRow/2; row-- {
-		for col := 0 - maxCol/2; col < maxCol/2; col++ {
-			if f(float64(col), float64(row)) {
+	for row := int(size.Row) / 2; row > 0-int(size.Row)/2; row-- {
+		for col := 0 - int(size.Col)/2; col < int(size.Col)/2; col++ {
+			var (
+				y = float64(row) * g.YScale
+				x = float64(col) * g.XScale
+			)
+			if fc(x, y) {
 				fmt.Print("*")
+			} else if x == 0 && y == 0 {
+				fmt.Print(g.Edge)
+			} else if x == 0 {
+				if y == math.Floor(y) {
+					fmt.Print(g.Edge.Scale)
+				} else {
+					fmt.Print(g.Edge.Y)
+				}
+			} else if y == 0 {
+				if x == math.Floor(x) {
+					fmt.Print(g.Edge.Scale)
+				} else {
+					fmt.Print(g.Edge.X)
+				}
 			} else {
 				fmt.Print(" ")
 			}
--- funny/heart.go
+++ funny/heart.go
@@ -1,16 +1,1 @@
 package funny
-
-import (
-	"math"
-)
-
-func Heart() error {
-	return Draw(func(variables ...float64) bool {
-		if len(variables) < 2 {
-			return false
-		}
-		xf := variables[0] / 20
-		yf := variables[1] / 20
-		return math.Pow(math.Pow(xf, 2)+math.Pow(yf, 2)-1, 3)-math.Pow(xf, 2)*math.Pow(yf, 3) <= 0
-	})
-}
