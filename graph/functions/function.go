fac45ba0585fa55230ca6e2ff2fbdbfea3941610
BYT0723 <twang9739@gmail.com>
2024-03-08 23:37:33 +0800

    perf: graph function

--- graph/functions/function.go
+++ graph/functions/function.go
@@ -1,23 +1,36 @@
 package functions
 
-import "math"
+import (
+	"math"
+)
 
 type BinaryFunction func(x, y float64) bool
 
 // 爱心
-func Love(x, y float64) bool {
-	return math.Pow(math.Pow(x, 2)+math.Pow(y, 2)-1, 3)-math.Pow(x, 2)*math.Pow(y, 3) <= 0
+func Love() BinaryFunction {
+	return func(x, y float64) bool {
+		return math.Pow(math.Pow(x, 2)+math.Pow(y, 2)-1, 3)-math.Pow(x, 2)*math.Pow(y, 3) <= 0
+	}
 }
 
 // 循环爱心
-func CircularLove(x, y float64) bool {
-	return y <= math.Sqrt(math.Abs(math.Sin(x)))+math.Sqrt(math.Abs(math.Cos(x))) && y >= math.Sqrt(math.Abs(math.Sin(x)))-math.Sqrt(math.Abs(math.Cos(x)))
+func CircularLove() BinaryFunction {
+	return func(x, y float64) bool {
+		return y <= math.Sqrt(math.Abs(math.Sin(x)))+math.Sqrt(math.Abs(math.Cos(x))) && y >= math.Sqrt(math.Abs(math.Sin(x)))-math.Sqrt(math.Abs(math.Cos(x)))
+	}
 }
 
-func RoseLine(x, y float64) bool {
-	theta := math.Atan2(y, x)
-	r := 2 * math.Sin(4*theta)
-	xp := r * math.Cos(theta)
-	yp := r * math.Sin(theta)
-	return math.Abs(x) <= math.Abs(xp) && math.Abs(y) <= math.Abs(yp)
+func RoseLine(leaf, scale float64) BinaryFunction {
+	return func(x, y float64) bool {
+		var (
+			theta = math.Atan2(y, x)
+			r     = scale * math.Cos(leaf*theta)
+			xp    = r * math.Cos(theta)
+			yp    = r * math.Sin(theta)
+		)
+		return (x >= 0 && y >= 0 && x <= xp && y <= yp) ||
+			(x <= 0 && y >= 0 && x >= xp && y <= yp) ||
+			(x <= 0 && y <= 0 && x >= xp && y >= yp) ||
+			(x >= 0 && y <= 0 && x <= xp && y >= yp)
+	}
 }
