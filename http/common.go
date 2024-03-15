dba4ed916e67627f9147eb8dcc9f73c5c332373f
BYT0723 <twang9739@gmail.com>
2024-03-15 15:42:43 +0800

    perf: http default client export

--- http/common.go
+++ http/common.go
@@ -6,31 +6,31 @@ import (
 	"time"
 )
 
-var defaultClient = NewClient()
+var DefaultClient = NewClient()
 
 func Get(rawUrl string, header http.Header, data any) (code int, body []byte, err error) {
-	return defaultClient.Get(context.Background(), rawUrl, header, data)
+	return DefaultClient.Get(context.Background(), rawUrl, header, data)
 }
 
 func GetWithContext(ctx context.Context, rawUrl string, header http.Header, data any) (code int, body []byte, err error) {
-	return defaultClient.Get(ctx, rawUrl, header, data)
+	return DefaultClient.Get(ctx, rawUrl, header, data)
 }
 
 func GetTimeout(rawUrl string, header http.Header, data any, timeout time.Duration) (code int, resp []byte, err error) {
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
-	return defaultClient.Get(ctx, rawUrl, header, data)
+	return DefaultClient.Get(ctx, rawUrl, header, data)
 }
 
 func GetAny[T any](rawUrl string, header http.Header, data any) (code int, resp *T, err error) {
 	resp = new(T)
-	code, err = defaultClient.GetAny(context.Background(), rawUrl, header, data, resp)
+	code, err = DefaultClient.GetAny(context.Background(), rawUrl, header, data, resp)
 	return
 }
 
 func GetAnyWithContext[T any](ctx context.Context, rawUrl string, header http.Header, data any) (code int, resp *T, err error) {
 	resp = new(T)
-	code, err = defaultClient.GetAny(ctx, rawUrl, header, data, resp)
+	code, err = DefaultClient.GetAny(ctx, rawUrl, header, data, resp)
 	return
 }
 
@@ -38,33 +38,33 @@ func GetAnyTimeout[T any](rawUrl string, header http.Header, data any, timeout t
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 	resp = new(T)
-	code, err = defaultClient.GetAny(ctx, rawUrl, header, data, resp)
+	code, err = DefaultClient.GetAny(ctx, rawUrl, header, data, resp)
 	return
 }
 
 func Post(rawUrl string, header http.Header, data any) (code int, body []byte, err error) {
-	return defaultClient.Post(context.Background(), rawUrl, header, data)
+	return DefaultClient.Post(context.Background(), rawUrl, header, data)
 }
 
 func PostWithContext(ctx context.Context, rawUrl string, header http.Header, data any) (code int, body []byte, err error) {
-	return defaultClient.Post(ctx, rawUrl, header, data)
+	return DefaultClient.Post(ctx, rawUrl, header, data)
 }
 
 func PostTimeout(rawUrl string, header http.Header, data any, timeout time.Duration) (code int, body []byte, err error) {
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
-	return defaultClient.Post(ctx, rawUrl, header, data)
+	return DefaultClient.Post(ctx, rawUrl, header, data)
 }
 
 func PostAny[T any](rawUrl string, header http.Header, data any) (code int, resp *T, err error) {
 	resp = new(T)
-	code, err = defaultClient.PostAny(context.Background(), rawUrl, header, data, resp)
+	code, err = DefaultClient.PostAny(context.Background(), rawUrl, header, data, resp)
 	return
 }
 
 func PostAnyWithContext[T any](ctx context.Context, rawUrl string, header http.Header, data any, timeout time.Duration) (code int, resp *T, err error) {
 	resp = new(T)
-	code, err = defaultClient.PostAny(ctx, rawUrl, header, data, resp)
+	code, err = DefaultClient.PostAny(ctx, rawUrl, header, data, resp)
 	return
 }
 
@@ -72,6 +72,6 @@ func PostAnyTimeout[T any](rawUrl string, header http.Header, data any, timeout 
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 	resp = new(T)
-	code, err = defaultClient.PostAny(ctx, rawUrl, header, data, resp)
+	code, err = DefaultClient.PostAny(ctx, rawUrl, header, data, resp)
 	return
 }
