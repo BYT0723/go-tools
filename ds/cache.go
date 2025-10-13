b2e5eeafc33014d3904f2a3c71da39b4957fb561
BYT0723 <twang9739@gmail.com>
2025-10-13 14:44:10 +0800

    perf: ds cache

--- ds/cache.go
+++ ds/cache.go
@@ -1,7 +1,7 @@
 package ds
 
 import (
-	"runtime"
+	"context"
 	"sync"
 	"time"
 )
@@ -11,37 +11,40 @@ type (
 		l       sync.Mutex
 		expire  time.Duration
 		cleanup time.Duration
-		entries map[string]*entry[T]
+		entries map[string]*cacheEntry[T]
+		ctx     context.Context
+		cf      context.CancelFunc
 	}
-	entry[T any] struct {
+	cacheEntry[T any] struct {
 		value      T
 		expireTime time.Time
 	}
 )
 
+// NewCache 新建缓存
+// expire 0 表示不过期
+// cleanup 0 表示不定时清理
 func NewCache[T any](expire, cleanup time.Duration) *Cache[T] {
 	c := &Cache[T]{
-		entries: make(map[string]*entry[T]),
+		entries: make(map[string]*cacheEntry[T]),
 		expire:  max(0, expire),
-		cleanup: cleanup,
+		cleanup: max(0, cleanup),
 	}
+	c.ctx, c.cf = context.WithCancel(context.Background())
 
 	if c.cleanup > 0 {
-		exit := make(chan struct{})
 		go func() {
 			t := time.NewTicker(c.cleanup)
+			defer t.Stop()
 			for {
 				select {
 				case <-t.C:
 					c.cleanExpireKey()
-				case <-exit:
+				case <-c.ctx.Done():
 					return
 				}
 			}
 		}()
-		runtime.SetFinalizer(c, func(c *Cache[T]) {
-			exit <- struct{}{}
-		})
 	}
 	return c
 }
@@ -64,7 +67,7 @@ func (c *Cache[T]) Get(key string) (value T, loaded bool) {
 
 	e, ok := c.entries[key]
 	if !ok || (!e.expireTime.IsZero() && e.expireTime.Before(time.Now())) {
-		return
+		return value, loaded
 	}
 	return e.value, true
 }
@@ -84,7 +87,7 @@ func (c *Cache[T]) SetWithExpire(key string, value T, expire time.Duration) {
 			e.expireTime = time.Now().Add(expire)
 		}
 	} else {
-		e = &entry[T]{value: value}
+		e = &cacheEntry[T]{value: value}
 		if expire > 0 {
 			e.expireTime = time.Now().Add(expire)
 		}
@@ -96,4 +99,14 @@ func (c *Cache[T]) Delete(key string) {
 	c.l.Lock()
 	defer c.l.Unlock()
 	delete(c.entries, key)
+}
+
+// Release releases the goroutine for scheduled cleaning
+func (c *Cache[T]) Release() error {
+	if c.cf != nil {
+		c.cf()
+		c.ctx = nil
+		c.cf = nil
+	}
+	return nil
 }
