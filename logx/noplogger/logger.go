c0e30e1fcd7bbc52b09f19af296f1f9636850ac9
BYT0723 <twang9739@gmail.com>
2026-01-17 23:03:44 +0800

    perf: service

---
+++ logx/noplogger/logger.go
@@ -0,0 +1,59 @@
+package noplogger
+
+import "github.com/BYT0723/go-tools/logx/logcore"
+
+type NopLogger struct{}
+
+func (l NopLogger) With(kvs ...logcore.Field) logcore.Logger {
+	return l
+}
+
+func (NopLogger) Debug(msg string, kvs ...logcore.Field) {
+}
+
+func (NopLogger) Debugf(format string, args ...any) {
+}
+
+func (NopLogger) Info(msg string, kvs ...logcore.Field) {
+}
+
+func (NopLogger) Infof(format string, args ...any) {
+}
+
+func (NopLogger) Warn(msg string, kvs ...logcore.Field) {
+}
+
+func (NopLogger) Warnf(format string, args ...any) {
+}
+
+func (NopLogger) Error(msg string, kvs ...logcore.Field) {
+}
+
+func (NopLogger) Errorf(format string, args ...any) {
+}
+
+func (NopLogger) Panic(msg string, kvs ...logcore.Field) {
+}
+
+func (NopLogger) Panicf(format string, args ...any) {
+}
+
+func (NopLogger) Fatal(msg string, kvs ...logcore.Field) {
+}
+
+func (NopLogger) Fatalf(format string, args ...any) {
+}
+
+func (NopLogger) Log(level string, msg string, kvs ...logcore.Field) {
+}
+
+func (NopLogger) Logf(level string, format string, args ...any) {
+}
+
+func (NopLogger) Sync() error {
+	return nil
+}
+
+func (l NopLogger) AddCallerSkip(caller int) logcore.Logger {
+	reutrn l
+}
--- srvx/service.go
+++ srvx/service.go
@@ -4,22 +4,33 @@ import (
 	"context"
 	"sync"
 
-	ctxx "github.com/BYT0723/go-tools/contextx"
 	"github.com/BYT0723/go-tools/logx"
+	"github.com/BYT0723/go-tools/logx/noplogger"
 )
 
 type Service interface {
 	Name() string
-	// 非阻塞式运行
-	// 可以从ctx中获取WaitGroup去启动goroutine防止阻塞
-	Start(ctx context.Context) error
-	ErrHandle(ctx context.Context, err error)
+
+	// Init 初始化资源
+	// 返回 error 表示服务无法启动
+	Init(ctx context.Context) error
+
+	// Run 启动后台任务
+	// Run 只在 Init 成功后调用
+	// 返回 error 表示服务提前异常退出
+	Run(ctx context.Context) error
+
+	// Destroy 释放资源
+	// 只会在 Init 成功后被调用
+	// 必须幂等
+	Destroy(ctx context.Context) error
 }
 
 type Services struct {
+	Log logx.Logger
+
 	wg       sync.WaitGroup
 	services []Service
-	Log      logx.Logger
 }
 
 func (ss *Services) Register(s Service) {
@@ -28,13 +39,32 @@ func (ss *Services) Register(s Service) {
 
 // Run start all service and wait for all service exit
 func (ss *Services) Run(ctx context.Context) {
-	ctx = ctxx.WithWaitGroup(ctx, &ss.wg)
-	for _, s := range ss.services {
-		if err := s.Start(ctx); err != nil {
-			s.ErrHandle(ctx, err)
-		}
-		if ss.Log != nil {
-			ss.Log.Infof("service %s run success", s.Name())
-		}
+	if ss.Log == nil {
+		ss.Log = noplogger.NopLogger{}
 	}
+	for _, s := range ss.services {
+		ss.wg.Go(func() {
+			name := s.Name()
+
+			ss.Log.Info("service init", logx.String("name", name))
+			if err := s.Init(ctx); err != nil {
+				ss.Log.Error("service init error", logx.String("name", name), logx.Err(err))
+				return
+			}
+
+			defer func() {
+				ss.Log.Error("service exit", logx.String("name", name))
+				if err := s.Destroy(ctx); err != nil {
+					ss.Log.Error("service destroy error", logx.String("name", name), logx.Err(err))
+				}
+			}()
+
+			ss.Log.Info("service run", logx.String("name", name))
+			if err := s.Run(ctx); err != nil {
+				ss.Log.Error("service run error", logx.String("name", name), logx.Err(err))
+				return
+			}
+		})
+	}
+	ss.wg.Wait()
 }
