4452a94259f08eb9844242c4f9ebd0f559f0a8f1
BYT0723 <twang9739@gmail.com>
2026-01-23 10:40:59 +0800

    rename Bytes and String to UnsafeBytes and UnsafeString for clarity

--- unsafex/bytes.go
+++ unsafex/bytes.go
@@ -4,14 +4,14 @@ import (
 	"unsafe"
 )
 
-// Bytes(string)
+// UnsafeBytes(string)
 // 新建byte slice header, 将Data指向字符串的Data内存地址
 // NOTE: 注意，如果string data内存非配在只读段内，修改[]byte内容会导致panic
-func Bytes(s string) []byte {
+func UnsafeBytes(s string) []byte {
 	return unsafe.Slice(unsafe.StringData(s), len(s))
 }
 
-// String([]byte)
+// UnsafeString([]byte)
 // 将byte slice header直接转换成string header
 //
 //	type SliceHeader struct {
@@ -26,6 +26,6 @@ func Bytes(s string) []byte {
 //	}
 //
 // 利用内存布局相似的特性，直接将byte slice header转换成string header使用
-func String(bs []byte) string {
+func UnsafeString(bs []byte) string {
 	return *(*string)(unsafe.Pointer(&bs))
 }
